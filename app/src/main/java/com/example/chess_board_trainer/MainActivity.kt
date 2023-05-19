fccaacc6fc6ebb6c66d746150a7c68297aa8e129
Deividson Luiz Okopnik <86014741+deividsonokopnik@users.noreply.github.com>
2023-05-19 19:07:50 -0300

    funcionando com timer e voz

--- app/src/main/java/com/example/chess_board_trainer/MainActivity.kt
+++ app/src/main/java/com/example/chess_board_trainer/MainActivity.kt
@@ -11,19 +11,36 @@ import android.media.MediaPlayer
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     val delay: Long = 1000000000000000
-    val interval: Long = 5000
+    val interval: Long = 6000
 
     private val timer = object: CountDownTimer(delay, interval) {
         override fun onTick(millisUntilFinished: Long) {
             val textView: TextView = findViewById(R.id.textView3) as TextView
-            val rnd1 = (1..8).random()
-            val rnd2 = listOf(('A'..'H')).flatten().random()
-            textView.text = rnd1.toString()+rnd2.toString()
+            val rnd1 = listOf(('A'..'H')).flatten().random()
+            val rnd2 = (1..8).random()
+            textView.text = rnd1.toString() + rnd2.toString()
 
-            var mediaPlayer = MediaPlayer.create(baseContext, R.raw.sa)
+            var mediaPlayer = MediaPlayer.create(baseContext, R.raw.sa);
+            if(rnd1.toString()=="B") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.sb) }
+            if(rnd1.toString()=="C") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.sc) }
+            if(rnd1.toString()=="D") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.sd) }
+            if(rnd1.toString()=="E") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.se) }
+            if(rnd1.toString()=="F") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.sf) }
+            if(rnd1.toString()=="G") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.sg) }
+            if(rnd1.toString()=="H") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.sh) }
+
             mediaPlayer.start()
             Thread.sleep(800)
+
             mediaPlayer = MediaPlayer.create(baseContext, R.raw.s1)
+            if(rnd2.toString()=="2") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.s2) }
+            if(rnd2.toString()=="3") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.s3) }
+            if(rnd2.toString()=="4") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.s4) }
+            if(rnd2.toString()=="5") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.s5) }
+            if(rnd2.toString()=="6") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.s6) }
+            if(rnd2.toString()=="7") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.s7) }
+            if(rnd2.toString()=="8") { mediaPlayer = MediaPlayer.create(baseContext, R.raw.s8) }
+
             mediaPlayer.start()
         }
 
